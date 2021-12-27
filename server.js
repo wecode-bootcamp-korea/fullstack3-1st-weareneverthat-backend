7d3059a6a120b1150848e3457755c7c5ad0ffef0
TaeYeongJ <dev.taeyeong@gmail.com>
2021-12-27 16:02:16 +0900

    backend 초기 세팅

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log('server is listening on PORT 8000');
+});

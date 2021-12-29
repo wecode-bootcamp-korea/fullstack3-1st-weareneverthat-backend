c6e45c7628696c9df16aaad9c1b63e80d4571a93
TaeYeong Jeong <dev.taeyeong@gmail.com>
2021-12-29 13:11:26 +0900

    Feature/modeling (#14)
    * Modify : 프리즈마 스키마 수정
    
    * prisma schema 모델링 변경
    
    * prisma schema 모델링 변경
    
    * 초기 세팅
    
    * 마이그레이션 폴더 삭제
    
    * prisma 모델링 수정
    
    * prisma 모델링 수정

---
+++ data.sql
@@ -0,0 +1,8 @@
+-- insert products data
+INSERT INTO categories(name) VALUES ('Outerwear'), ('Sweatshirts'), ('Tees'), ('Shirts'), ('Tops'), ('Bottoms'), ('Shorts'), ('Headwear'), ('Bags'), ('Shoes'), ('Accessories'), ('Collaboration');
+
+-- insert product groups data
+INSERT INTO product_groups(name, price, discount_price, description, country, categoryId) VALUES ('PERTEX T Down Jacket', 189000, 132300, 'PERTEX QUANTUM fabric Duck down 80%, Duck Feather 20% Logo embroidery on left chest and wrist Logo jacquard zip puller Logo print above the inner pocket Nylon 100%', 'Vietnam', 1);
+
+-- insert product data
+INSERT INTO products(name, color, product_group_id) VALUES ()

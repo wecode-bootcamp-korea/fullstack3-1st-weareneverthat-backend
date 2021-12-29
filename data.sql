7473977371359b066579eae949f1ac9dfbca23c9
guminki <rnalsrl93@gmail.com>
2021-12-29 16:53:48 +0900

    data 생성

--- data.sql
+++ data.sql
@@ -1,8 +1,51 @@
+INSERT INTO categories VALUES (1,'Outerwear'),(2,'Sweatshirts'),(3,'Tees'),(4,'Shirts'),(5,'Tops'),(6,'Bottoms'),(7,'Shorts'),(8,'Headwear'),(9,'Bags'),(10,'Shoes'),(11,'Accessories'),(12,'Collaboration');
+
+-- insert product colors data
+INSERT INTO product_colors(color) VALUES ('Black'), ('Blue'), ('Navy');
+
+-- insert product sizes data
+INSERT INTO product_sizes(size) VALUES ('S'), ('M'), ('L');
+
 -- insert products data
-INSERT INTO categories(name) VALUES ('Outerwear'), ('Sweatshirts'), ('Tees'), ('Shirts'), ('Tops'), ('Bottoms'), ('Shorts'), ('Headwear'), ('Bags'), ('Shoes'), ('Accessories'), ('Collaboration');
+INSERT INTO 
+  products(name, price, discount_price, description, country, category_id)
+VALUES 
+  ('PERTEX T Down Jacked', 189000, 132300, 'PERTEX QUANTUM fabric', 'Vietnam', 1),
+  ('T-Logo Hoodie', 82000, 57400, 'Logo embroidery on front', 'Bangladesh', 2),
+  ('Converse x thisisneverthat Tee', 55000, 55000, '240GSM oversized cotton jersey with modern, oversized fit', 'China', 3);
 
--- insert product groups data
-INSERT INTO product_groups(name, price, discount_price, description, country, categoryId) VALUES ('PERTEX T Down Jacket', 189000, 132300, 'PERTEX QUANTUM fabric Duck down 80%, Duck Feather 20% Logo embroidery on left chest and wrist Logo jacquard zip puller Logo print above the inner pocket Nylon 100%', 'Vietnam', 1);
+-- insert product details data
+INSERT INTO
+  product_details(quantity, product_id, product_color_id, product_size_id)
+VALUES
+  ('10', 1, 1, 1),
+  ('20', 1, 1, 2),
+  ('30', 1, 1, 3),
+  ('10', 1, 3, 1),
+  ('20', 1, 3, 2),
+  ('30', 1, 3, 3),
+  ('10', 2, 1, 1),
+  ('20', 2, 1, 2),
+  ('30', 2, 1, 3),
+  ('10', 2, 2, 1),
+  ('20', 2, 2, 2),
+  ('30', 2, 2, 3),
+  ('10', 3, 2, 1),
+  ('20', 3, 2, 2),
+  ('30', 3, 2, 3),
+  ('10', 3, 3, 1),
+  ('20', 3, 3, 2),
+  ('30', 3, 3, 3);
 
--- insert product data Black Blue Navy Sage Gray
-INSERT INTO products(color, product_group_id) VALUES ('black', 1), ('')
+-- insert product images data
+-- INSERT INTO
+--   product_images(image_url, )
+
+-- insert users data
+INSERT INTO
+  users(email, password, name)
+VALUES
+  ('dev.taeyeong', '1234', 'TaeYeong'),
+  ('dev.minsu', '1234', 'MinSu'),
+  ('dev.mingi', '1234', 'MinGi'),
+  ('dev.sky', '1234', 'Sky');
--- models/productDao.js
+++ models/productDao.js
@@ -16,10 +16,25 @@ const getDetailById = async id => {
 	const detailInfo = await prisma.$queryRaw`
     SELECT
       product_details.id,
-      
+      count,
+      colors.color,
+      size
+    FROM product_details
+    JOIN products ON product_details.products_id = products.id
+    WHERE products.id = ${id}
   `;
 
-	return productInfo;
+	const detailImage = await prisma.$queryRaw`
+    SELECT
+      image_url,
+      is_list
+    FROM product_images
+    JOIN product_details ON product_images.product_id = product_details.id
+    JOIN products ON product_detail.products_id = products.id
+    WHERE products.id = ${id}
+  `;
+
+	return { productInfo, detailInfo, detailImage };
 };
 
 module.exports = { getDetailById };

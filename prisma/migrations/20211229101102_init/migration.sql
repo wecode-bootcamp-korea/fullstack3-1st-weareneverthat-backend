c776c861138c647c7834bbdecdbdbed3ee090bfe
TaeYeong Jeong <dev.taeyeong@gmail.com>
2021-12-29 19:32:02 +0900

    prisma 모델링 수정 (#18)

--- prisma/migrations/20211229101102_init/migration.sql
+++ prisma/migrations/20211229101102_init/migration.sql
@@ -9,13 +9,13 @@ CREATE TABLE `categories` (
 -- CreateTable
 CREATE TABLE `products` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
     `price` INTEGER NOT NULL,
     `discount_price` INTEGER NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `country` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `cateory_id` INTEGER NOT NULL,
+    `category_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -26,7 +26,6 @@ CREATE TABLE `product_details` (
     `quantity` INTEGER NOT NULL,
     `product_id` INTEGER NOT NULL,
     `product_color_id` INTEGER NOT NULL,
-    `product_size_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -58,6 +57,15 @@ CREATE TABLE `product_sizes` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `details_sizes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `product_detail_id` INTEGER NOT NULL,
+    `product_size_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
@@ -73,7 +81,7 @@ CREATE TABLE `users` (
 -- CreateTable
 CREATE TABLE `carts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `product_detail_id` INTEGER NOT NULL,
+    `detail_size_id` INTEGER NOT NULL,
     `user_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
@@ -89,7 +97,7 @@ CREATE TABLE `hearts` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `products` ADD CONSTRAINT `products_cateory_id_fkey` FOREIGN KEY (`cateory_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `products` ADD CONSTRAINT `products_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `product_details` ADD CONSTRAINT `product_details_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -98,13 +106,16 @@ ALTER TABLE `product_details` ADD CONSTRAINT `product_details_product_id_fkey` F
 ALTER TABLE `product_details` ADD CONSTRAINT `product_details_product_color_id_fkey` FOREIGN KEY (`product_color_id`) REFERENCES `product_colors`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `product_details` ADD CONSTRAINT `product_details_product_size_id_fkey` FOREIGN KEY (`product_size_id`) REFERENCES `product_sizes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE `product_images` ADD CONSTRAINT `product_images_product_detail_id_fkey` FOREIGN KEY (`product_detail_id`) REFERENCES `product_details`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `carts` ADD CONSTRAINT `carts_product_detail_id_fkey` FOREIGN KEY (`product_detail_id`) REFERENCES `product_details`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `details_sizes` ADD CONSTRAINT `details_sizes_product_detail_id_fkey` FOREIGN KEY (`product_detail_id`) REFERENCES `product_details`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `details_sizes` ADD CONSTRAINT `details_sizes_product_size_id_fkey` FOREIGN KEY (`product_size_id`) REFERENCES `product_sizes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `carts` ADD CONSTRAINT `carts_detail_size_id_fkey` FOREIGN KEY (`detail_size_id`) REFERENCES `details_sizes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `carts` ADD CONSTRAINT `carts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

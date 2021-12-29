2502612093ec780633343e77bfb9b9dcc909e5b0
TaeYeongJ <dev.taeyeong@gmail.com>
2021-12-29 15:01:29 +0900

    prisma 모델링 수정

--- data.sql
+++ data.sql
@@ -4,5 +4,5 @@ INSERT INTO categories(name) VALUES ('Outerwear'), ('Sweatshirts'), ('Tees'), ('
 -- insert product groups data
 INSERT INTO product_groups(name, price, discount_price, description, country, categoryId) VALUES ('PERTEX T Down Jacket', 189000, 132300, 'PERTEX QUANTUM fabric Duck down 80%, Duck Feather 20% Logo embroidery on left chest and wrist Logo jacquard zip puller Logo print above the inner pocket Nylon 100%', 'Vietnam', 1);
 
--- insert product data
-INSERT INTO products(name, color, product_group_id) VALUES ()
+-- insert product data Black Blue Navy Sage Gray
+INSERT INTO products(color, product_group_id) VALUES ('black', 1), ('')

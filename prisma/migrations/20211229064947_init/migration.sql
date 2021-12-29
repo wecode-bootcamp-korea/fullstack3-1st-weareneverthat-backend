010b05427baa1a6b345c4ad7a0d0343d4b2f45e2
minsukang <minsu910725@gmail.com>
2021-12-29 19:33:48 +0900

    모델링 수정

--- prisma/migrations/20211229064947_init/migration.sql
+++ prisma/migrations/20211229064947_init/migration.sql
@@ -9,13 +9,13 @@ CREATE TABLE `categories` (
 -- CreateTable
 CREATE TABLE `products` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
     `price` INTEGER NOT NULL,
     `discount_price` INTEGER NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `country` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `cateory_id` INTEGER NOT NULL,
+    `category_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -89,7 +89,7 @@ CREATE TABLE `hearts` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `products` ADD CONSTRAINT `products_cateory_id_fkey` FOREIGN KEY (`cateory_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `products` ADD CONSTRAINT `products_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `product_details` ADD CONSTRAINT `product_details_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

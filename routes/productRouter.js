1dfd340da85f84ebee617902aded559f014344f7
TaeYeongJ <dev.taeyeong@gmail.com>
2021-12-31 15:50:57 +0900

    ADD: product list api (category, price sort)

---
+++ routes/productRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const productController = require('../controllers/productController');
+
+router.get('/:category', productController.productList);
+
+module.exports = router;

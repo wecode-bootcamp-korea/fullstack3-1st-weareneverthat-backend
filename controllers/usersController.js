a99c10f1f4ef2975bf71e341ec844f53b8a0373c
guminki <rnalsrl93@gmail.com>
2022-01-06 15:40:40 +0900

    .

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -22,6 +22,8 @@ const signIn = async (req, res) => {
 	try {
 		const { email, password } = req.body;
 		const REQUIRED_KEYS = { email, password };
+		console.log(email);
+		console.log(password);
 		for (let key in REQUIRED_KEYS) {
 			if (!REQUIRED_KEYS[key]) {
 				return res.status(400).json({ message: 'KEY_ERROR' });
